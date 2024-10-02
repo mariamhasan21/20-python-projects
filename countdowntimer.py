709e4051220c13a30e794c9d8250df7a95410610
Mariam Hasan <86767403+toptaekkug@users.noreply.github.com>
2024-10-02 22:10:52 +0600

    Countdown Timer
    This countdown timer is capable of counting the time you provide it. I have used the time module.

---
+++ countdowntimer.py
@@ -0,0 +1,9 @@
+import time
+my_time = int(input("Enter the timer: "))
+for x in range(my_time, 0, -1):
+    seconds = int(x%60)
+    minutes = int(x/1200)
+    hours = int(x/3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+print("Yippe Done!")
