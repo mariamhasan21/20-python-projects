721b90081ab815b907492af21196fae45a254a2a
Mariam Hasan <86767403+mariamhasan21@users.noreply.github.com>
2024-11-01 15:53:04 +0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,17 +1,34 @@
-print("__________Welcome to the Calculator__________".center(50))
-n1 = float(input("Hey insert your first number: "))
-n2 = float(input("Hey insert your second number: "))
-operator = input("Select the operator: '+ - * /': ")
-if operator == "+":
-    r = (n1+n2)
-    print(r)
-elif operator == "-":
-    r = (n1-n2)
-    print(r)
-elif operator == "*":
-    r = (n1*n2)
-    print(r)
+print("-----------------------Welcome to the Calculator-----------------------")
+print("-----------------------------------------------------------------------")
+running = True
 
-elif operator == "/":
-    r = (n1/n2)
-    print(r)
+while running:
+    n1 = float(input("Insert your first number: "))
+    n2 = float(input("Insert your second number: "))
+  
+    user_operator = input("1. Addition 2. Subtraction \n3. Multiplication 4. Division\nSelect the operator(1,2,3,4): ")
+    operator = {1:'Addition', 2: 'Subtraction', 3: 'Multiplication', 4:'Division'}
+   
+    print("-----------------------------------------------------------------------")
+
+    if user_operator == "1": #Takes the keys from the dictionary
+        r = (n1+n2) #Formula
+        print(f"The addition of {n1}+{n2} = {r:.2f}")
+
+    elif user_operator == "2": 
+        r = (n1-n2)
+        print(f"The subtraction of {n1}-{n2} = {r:.2f}")
+
+    elif user_operator == "3":
+        r = (n1*n2)
+        print(f"The multiplication of {n1}*{n2} = {r:.2f}")
+
+
+    elif user_operator == "4":
+        r = (n1/n2)
+        print(f"The division of {n1}/{n2} = {r:.2f}")
+
+    if not input("Calculate again?(y/n): ") == "y": #Checks if the user wants to use the calculator again
+        running = False
+
+print("Thanks for using the calculator~")
