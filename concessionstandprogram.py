8469d07d16b25e8ca69adcda9a3b926d871e509e
Mariam Hasan <86767403+mariamhasan21@users.noreply.github.com>
2024-11-02 10:57:16 +0600

    Update concessionstandprogram.py
    This program allows users to select food items from a menu, adds chosen items to a cart, and calculates the total cost of the order.

--- concessionstandprogram.py
+++ concessionstandprogram.py
@@ -1,31 +1,41 @@
-menu = {
+#This program allows users to select food items from a menu, add chosen items to a cart, and calculate the total cost of the order.
+menu = { #Stored the menu list in a dictionary with their prices
     "potato":22,
-    "pizza":10,
-    "popcorn":5,
-    "chips":8,
+    "pizza": 12,
+    "chips": 5,
+    "macoroni cheese": 30,
+    "donuts": 5,
     "soda": 3,
     "lemonade":7,
-    "donuts":5
+    "popcorn":5,
 }
-cart = []
-total = 0
-print("-------MENU-------")
-for key, value in menu.items():
-    print(f"{key:10}: ${value:.2f}")
-print("------------------")
+cart = [] #at the initial state, the cart is empty
+total = 0 #total is also empty since no food is added
+print("------------MENU------------")
+
+for key, value in menu.items(): #for loop counts all of the food items from the menu dictionary
+    print(f"{key:20}: ${value:.2f}") #20 for {key:20} is used for space
+
+print("----------------------------")
 
 while True:
-    food = input("Enter the food (q to quit): ").lower()
-    if food == "q":
+    food = input("Enter the food you want to add to your cart (Press 'c' to see your final order) \nFood Name: ").strip().lower() 
+    if food == "c": #condition to end the program
         break
-    elif menu.get(food) is not None:
-        cart.append(food)
+    elif menu.get(food) is not None: #not None = negative+negative = positive.
+        #Menu dictionary is checking if it's food items matches with the users food items 
+        cart.append(food) #if the condition is true, the user's food items are added to the cart
+        
+        print(f"Added {food.capitalize()} to your cart.")
+    
     else:
-        print("Oopsie the food is not available on menu!")
-print("--------YOUR ORDER---------")
-for food in cart:
-    total += menu.get(food)
-    print(food, end =" ")
+        print("Oopsie the food is not available on menu!") 
+        #When the food is not to be found on the menu dictionary
 
-print()
-print(f"Your total is ${total:.2f}")
+print("-----------YOUR ORDER---------")
+
+for food in cart:
+    total += menu.get(food) #fetched the value of the menu dictionary since total was already an integer
+    print(food, end = " ")
+
+print(f"\nYour total is ${total:.2f}")
