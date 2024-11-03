3024fb5bf6a4daf83229d0ca9441ade8f24f41df
Mariam Hasan <86767403+mariamhasan21@users.noreply.github.com>
2024-11-03 20:14:18 +0600

    Count Down Timer

--- countdowntimer.py
+++ countdowntimer.py
@@ -1,9 +1,11 @@
+#Count down timer
 import time
 my_time = int(input("Enter the timer: "))
-for x in range(my_time, 0, -1):
-    seconds = int(x%60)
-    minutes = int(x/1200)
-    hours = int(x/3600)
+for x in range(my_time, 0, -1): #-1 jumps to the current time's previous one
+    seconds  = int(x%60) #gives the remainder seconds that don't make up a full minute
+    minutes = int(x// 60) %60
+    hours = int(x// 3600) #3600 seconds per hour
     print(f"{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
-print("Yippe Done!")
+print("Yippe, times up!")
+
