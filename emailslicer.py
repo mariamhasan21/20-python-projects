ad8c7707127355b8ee9e4fba7ecb929aef504355
Mariam Hasan <86767403+mariamhasan21@users.noreply.github.com>
2024-11-07 08:18:24 +0600

    Email Slicer

--- emailslicer.py
+++ emailslicer.py
@@ -1,5 +1,18 @@
-email = input("Enter your email: ")
+# Email Slicer
+# This email slicer slices the email into two parts. One for the username and second one for domain
 
-username = email[:email.index('@')]
-domain = email[email.index('@') + 1 : ]
-print(f"Your username is {username} and domain is {domain}")
+email = input("Enter your email: ").strip() #Used strip incase there are any white spaces
+
+# Use try except method to handle any potential errors
+
+if "@" in email and email.count("@") == 1: # Validate if '@' is present in  email and if it is available only for once
+
+    try:
+        username = email[:email.index('@')] #Print the statement from 0 index to before the @
+        domain = email[email.index('@') + 1: ] #Print the statement right after @ to the end
+        print(f"Your username is {username} and domain is {domain}")
+    except ValueError:
+        print("Invalid email input")
+
+else:
+    print("Invalid email format. Make sure there is one '@' symbol.")
