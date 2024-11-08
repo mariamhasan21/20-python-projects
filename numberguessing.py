fa5b6ca18ccb26b3398dcdd1187a891db0fe6da5
Mariam Hasan <86767403+mariamhasan21@users.noreply.github.com>
2024-11-08 08:36:36 +0600

    Number Guessing Program

--- numberguessing.py
+++ numberguessing.py
@@ -1,30 +1,43 @@
+#Number Guessing Program
+# This program uses random function and lets the user choose a number to try and guess
+
 import random
-lowest = 1
+#Defining the range from 0 to 100
+lowest = 1 
 highest = 100
-answer = random.randint(lowest, highest)
-guesses = 0
+answer = random.randint(lowest, highest) #Randint gives a range to random numbers
+guesses = 0 
 
+#Using while loop to make sure the program runs till the user guesses the correct number
 while True:
-    guess = input("Guess the number: ")
     try:
-        if guess.isdigit():
-            guess = int(guess)
-            guesses += 1
-
-        if guess < lowest or guess > highest:
+        guess = int(input("Enter the number: "))
+        guesses += 1 #This will keep the record of the user trying to guess the correct numeber
+        if answer == guess:
+            print("Correct 🎉")
+            print(f"Number of guesses: {guesses} times")
+            break
+        
+        elif guess < 0:
+            print("Number can't be negative")
+        
+        elif guess < lowest or guess > highest:
             print("Guess is out of range")
             print(f"Please select a number between {lowest} to {highest}")
-        elif guess > answer:
-            print("Too high! Check again")
-        elif guess < answer:
-            print("Too low! Check again")
+
+        elif answer < guess:
+            print("Too high")
+
+        elif answer > guess:
+            print("Too low")
+        
         else:
             print("Correct!")
             print(f"Number of guesses: {guesses} times")
             break
-    except ValueError:
-        print("Invalid input. Please enter a valid number.")
+
+    except ValueError: #Shows this message if user do not provide int input
+        print("Invalid input")
 
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        break
+        print(f"An unexpected error occured {e}")
